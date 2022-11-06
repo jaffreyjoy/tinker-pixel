f06c5ee0d7f9b9f7e1b8d57201b0cb1d08af90e4
Jaffrey <jaffreyjoy@gmail.com>
2022-11-06 19:31:11 +0530

    enh: add app logo

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,9 +1,11 @@
 import '../styles/TopBar.css'
+import appicon from '../assests/img/Icon.png';
 
 function TopBar() {
     return (
       <div className="TopBar">
-        <div><span className="heading">Tinker Pixel</span></div>
+        {/* <div><span className="heading">Tinker Pixel</span></div> */}
+        <div> <img  className="appIcon" src={appicon} alt="Tinker Pixel" /> </div>
       </div>
     );
   }
