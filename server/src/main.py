f23bc1312aa732b341ca7b4fe29e6e5fbf243a74
Prakhar Gupta <prakhar.g@students.iiit.ac.in>
2022-11-12 11:25:01 +0530

    /upload/filter_name route setup

--- server/src/main.py
+++ server/src/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/upload/{filter_name}")
+async def get_file_image(filter_name: str):
+    return filter_name
